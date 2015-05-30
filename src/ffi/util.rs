2094d355047e7ae513833632adac8d8da1ce03b7
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-05-30 16:29:47 +0100

    Remove redundant type annotations

--- src/ffi/util.rs
+++ src/ffi/util.rs
@@ -32,7 +32,7 @@ fn test_string_cstr() {
 pub fn get_h5_str<T, F>(func: F) -> H5Result<String>
                  where F: Fn(*mut c_char, size_t) -> T, T: Integer + NumCast {
     unsafe {
-        let len: isize = 1 + cast::<T, isize>(func(ptr::null_mut::<c_char>(), 0)).unwrap();
+        let len: isize = 1 + cast::<T, isize>(func(ptr::null_mut(), 0)).unwrap();
         ensure!(len > 0, "negative string length in get_h5_str()");
         let buf = libc::malloc((len as size_t) * mem::size_of::<c_char>() as size_t) as *mut c_char;
         func(buf, len as size_t);
@@ -47,7 +47,7 @@ pub fn test_get_h5_str() {
     use ffi::h5e::{H5E_type_t, H5Eget_msg, H5E_CANTOPENOBJ};
     let s = unsafe {
         get_h5_str(|msg, size| {
-            H5Eget_msg(*H5E_CANTOPENOBJ, ptr::null_mut::<H5E_type_t>(), msg, size)
+            H5Eget_msg(*H5E_CANTOPENOBJ, ptr::null_mut(), msg, size)
         }).ok().unwrap()
     };
     assert_eq!(s, "Can't open object");
