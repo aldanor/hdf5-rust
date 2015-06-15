eb8c1ff31998138a84f8dcf9e39c7e4860054468
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-06-15 02:34:14 +0100

    Add AnyDatatype trait, implement AnyDatatype::size(), add to prelude

--- src/datatype.rs
+++ src/datatype.rs
@@ -5,7 +5,7 @@ use object::Object;
 use ffi::h5i::{H5I_DATATYPE, hid_t};
 use ffi::h5t::{H5T_INTEGER, H5T_FLOAT, H5T_NO_CLASS, H5T_NCLASSES, H5T_ORDER_BE, H5T_ORDER_LE,
                H5T_SGN_2, H5Tcopy, H5Tget_class, H5Tget_order, H5Tget_offset, H5Tget_sign,
-               H5Tget_precision};
+               H5Tget_precision, H5Tget_size};
 
 #[cfg(target_endian = "big")]
 use globals::{
@@ -29,6 +29,16 @@ pub enum Datatype {
     Integer(IntegerDatatype),
     Float(FloatDatatype),
 }
+
+pub trait AnyDatatype: ID {
+    /// Get the total size of the datatype in bytes.
+    fn size(&self) -> usize {
+        h5call!(H5Tget_size(self.id())).unwrap_or(0) as usize
+    }
+}
+
+impl AnyDatatype for Datatype {}
+impl<T> AnyDatatype for T where T: AtomicDatatype {}
 
 macro_rules! def_atomic {
     ($name:ident, $h5t:ident) => (
@@ -169,7 +179,7 @@ impl Object for Datatype {}
 
 #[cfg(test)]
 mod tests {
-    use super::{Datatype, AtomicDatatype, ToDatatype};
+    use super::{Datatype, AnyDatatype, AtomicDatatype, ToDatatype};
     use super::Datatype::*;
     use handle::FromID;
     use ffi::h5i::H5I_INVALID_HID;
@@ -182,8 +192,8 @@ mod tests {
     #[cfg(target_endian = "little")] const IS_BE: bool = false;
     #[cfg(target_endian = "little")] const IS_LE: bool = true;
 
-    #[cfg(target_pointer_width = "32")] const POINTER_WIDTH: usize = 32;
-    #[cfg(target_pointer_width = "64")] const POINTER_WIDTH: usize = 64;
+    #[cfg(target_pointer_width = "32")] const POINTER_WIDTH_BYTES: usize = 4;
+    #[cfg(target_pointer_width = "64")] const POINTER_WIDTH_BYTES: usize = 8;
 
     #[test]
     pub fn test_invalid_datatype() {
@@ -194,7 +204,7 @@ mod tests {
     #[test]
     pub fn test_atomic_datatype() {
         macro_rules! test_integer {
-            ($tp:ty, $signed:expr, $precision:expr) => (
+            ($tp:ty, $signed:expr, $precision:expr, $size:expr) => (
                 match <$tp as ToDatatype>::to_datatype().unwrap() {
                     Datatype::Integer(dt) => {
                         assert_eq!(dt.is_be(), IS_BE);
@@ -202,6 +212,7 @@ mod tests {
                         assert_eq!(dt.offset(), 0);
                         assert_eq!(dt.precision(), $precision);
                         assert_eq!(dt.is_signed(), $signed);
+                        assert_eq!(dt.size(), $size);
                     },
                     _ => panic!("Integer datatype expected")
                 }
@@ -209,35 +220,36 @@ mod tests {
         }
 
         macro_rules! test_float {
-            ($tp:ty, $precision:expr) => (
+            ($tp:ty, $precision:expr, $size:expr) => (
                 match <$tp as ToDatatype>::to_datatype().unwrap() {
                     Datatype::Float(dt) => {
                         assert_eq!(dt.is_be(), IS_BE);
                         assert_eq!(dt.is_le(), IS_LE);
                         assert_eq!(dt.offset(), 0);
-                        assert_eq!(dt.precision(), $precision)
+                        assert_eq!(dt.precision(), $precision);
+                        assert_eq!(dt.size(), $size);
                     },
                     _ => panic!("Float datatype expected")
                 }
             )
         }
 
-        test_integer!(bool, false, 8);
+        test_integer!(bool, false, 8, 1);
 
-        test_integer!(i8, true, 8);
-        test_integer!(i16, true, 16);
-        test_integer!(i32, true, 32);
-        test_integer!(i64, true, 64);
+        test_integer!(i8, true, 8, 1);
+        test_integer!(i16, true, 16, 2);
+        test_integer!(i32, true, 32, 4);
+        test_integer!(i64, true, 64, 8);
 
-        test_integer!(u8, false, 8);
-        test_integer!(u16, false, 16);
-        test_integer!(u32, false, 32);
-        test_integer!(u64, false, 64);
+        test_integer!(u8, false, 8, 1);
+        test_integer!(u16, false, 16, 2);
+        test_integer!(u32, false, 32, 4);
+        test_integer!(u64, false, 64, 8);
 
-        test_float!(f32, 32);
-        test_float!(f64, 64);
+        test_float!(f32, 32, 4);
+        test_float!(f64, 64, 8);
 
-        test_integer!(isize, true, POINTER_WIDTH);
-        test_integer!(usize, false, POINTER_WIDTH);
+        test_integer!(isize, true, POINTER_WIDTH_BYTES * 8, POINTER_WIDTH_BYTES);
+        test_integer!(usize, false, POINTER_WIDTH_BYTES * 8, POINTER_WIDTH_BYTES);
     }
 }
