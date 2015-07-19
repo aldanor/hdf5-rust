a03b7446b6afcce7b7eb6fae92f310d9b87b26c9
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-07-19 21:16:45 +0100

    Remove all debug macros from macros module

--- src/macros.rs
+++ src/macros.rs
@@ -92,39 +92,3 @@ macro_rules! h5try_s {
 macro_rules! h5try {
     ($expr:expr) => (h5try_s!(unsafe { $expr }))
 }
-
-thread_local!(static THREAD_ID: () = ());
-
-pub fn thread_id() -> usize {
-    THREAD_ID.with(|x| x as *const _ as usize)
-}
-
-macro_rules! debug {
-    ($arg:expr) => {
-        {
-            use macros::thread_id;
-            println!("{:x}: {}:{}: {}", thread_id(), file!(), line!(), $arg);
-        }
-    };
-    ($fmt:expr, $($arg:tt)*) => {
-        {
-            use macros::thread_id;
-            println!("{:x}: {}:{}: {}", thread_id(), file!(), line!(), format!($fmt, $($arg)*));
-        }
-    };
-}
-
-macro_rules! debug_id {
-    ($id:expr) => {
-        {
-            use object::Object;
-            debug!("{} / {} / {}", ($id).id(), ($id).refcount(), ($id).is_valid());
-        }
-    };
-    ($id:expr, $msg:expr) => {
-        {
-            use object::Object;
-            debug!("{}: {} / {} / {}", $msg, ($id).id(), ($id).refcount(), ($id).is_valid());
-        }
-    };
-}
