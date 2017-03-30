26758478fbf8d421954125aef024324e22b13440
Ivan Smirnov <i.s.smirnov@gmail.com>
2017-03-30 21:54:04 +0100

    Impl H5Type for tuples only up to length 12

--- hdf5-types/src/h5type.rs
+++ hdf5-types/src/h5type.rs
@@ -205,7 +205,7 @@ macro_rules! impl_tuple {
     );
 }
 
-impl_tuple! { T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15 }
+impl_tuple! { A, B, C, D, E, F, G, H, I, J, K, L }
 
 unsafe impl<T: Array<Item=I> + 'static, I: H5Type> H5Type for T {
     #[inline]
