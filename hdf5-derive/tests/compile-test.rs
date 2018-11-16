d2978676adf9e99430b52a084a9764f3596317b3
Ivan Smirnov <i.s.smirnov@gmail.com>
2018-11-16 20:42:03 +0000

    Make compile-tests actually compile again

--- hdf5-derive/tests/compile-test.rs
+++ hdf5-derive/tests/compile-test.rs
@@ -1,18 +1,14 @@
-#![cfg(feature = "compiletest")]
-
-extern crate compiletest_rs as compiletest;
-
 use std::path::PathBuf;
 
 fn run_mode(mode: &'static str) {
-    let mut config = compiletest::default_config();
+    let mut config = compiletest_rs::Config::default();
     let cfg_mode = mode.parse().ok().expect("Invalid mode");
 
     config.target_rustcflags = Some("-Ltarget/debug/ -Ltarget/debug/deps/".to_owned());
     config.mode = cfg_mode;
     config.src_base = PathBuf::from(format!("tests/{}", mode));
 
-    compiletest::run_tests(&config);
+    compiletest_rs::run_tests(&config);
 }
 
 #[test]
