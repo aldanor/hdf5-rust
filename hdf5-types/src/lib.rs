0a4c0a3b1061240db009dfc1f84d880df06be9cc
Ivan Smirnov <i.s.smirnov@gmail.com>
2018-12-10 23:09:57 +0000

    Remove redundant ‘extern crate’

--- hdf5-types/src/lib.rs
+++ hdf5-types/src/lib.rs
@@ -1,9 +1,6 @@
 #![recursion_limit = "1024"]
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::transmute_bytes_to_str))]
 
-extern crate ascii;
-extern crate libc;
-
 #[macro_use]
 extern crate error_chain;
 
