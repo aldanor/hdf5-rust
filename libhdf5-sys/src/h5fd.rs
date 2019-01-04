f7dfd3efb046b34b5521a08e77c0b4b8aad87f94
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-01-04 19:39:40 +0000

    Remove H5FD_*_term() bindings (private anyway)

--- libhdf5-sys/src/h5fd.rs
+++ libhdf5-sys/src/h5fd.rs
@@ -295,14 +295,12 @@ extern "C" {
 // sec2 driver
 extern "C" {
     pub fn H5FD_sec2_init() -> hid_t;
-    pub fn H5FD_sec2_term();
     pub fn H5Pset_fapl_sec2(fapl_id: hid_t) -> herr_t;
 }
 
 // core driver
 extern "C" {
     pub fn H5FD_core_init() -> hid_t;
-    pub fn H5FD_core_term();
     pub fn H5Pset_fapl_core(fapl_id: hid_t, increment: size_t, backing_store: hbool_t) -> herr_t;
     pub fn H5Pget_fapl_core(
         fapl_id: hid_t, increment: *mut size_t, backing_store: *mut hbool_t,
@@ -312,7 +310,6 @@ extern "C" {
 // stdio driver
 extern "C" {
     pub fn H5FD_stdio_init() -> hid_t;
-    pub fn H5FD_stdio_term();
     pub fn H5Pset_fapl_stdio(fapl_id: hid_t) -> herr_t;
 }
 
