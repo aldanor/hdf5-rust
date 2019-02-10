1e79f95b5c63f18de5685f91c6e6edc602122ed4
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-02-10 14:20:50 +0000

    Add helper macros for plist testing (+mashup dep)

--- tests/test_plist.rs
+++ tests/test_plist.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate mashup;
+
 use std::mem;
 
 use libhdf5_sys::h5::hsize_t;
@@ -33,6 +36,70 @@ macro_rules! test_plist {
         let pl_copy = $builder::from_plist(&pl)?.finish()?;
         assert_eq!(pl, pl_copy);
     }
+}
+
+macro_rules! test_pl {
+    ($ty:ident, $field:ident ($($arg:expr),+): $($name:ident=$value:expr),+) => (
+        test_pl!($ty, $field ($($arg,)+): $($name=$value,)+)
+    );
+
+    ($ty:ident, $field:ident ($($arg:expr,)+): $($name:ident=$value:expr,)+) => ({
+        let mut b = $ty::build();
+        mashup! { m["get" $field] = get_ $field; }
+        b.$field($($arg,)+);
+        let fapl = b.finish()?;
+        $(assert_eq!(fapl.$field().$name, $value);)+
+        m! { $(assert_eq!(fapl."get" $field()?.$name, $value);)+ }
+    });
+
+    ($ty:ident, $field:ident: $($name:ident=$value:expr),+) => (
+        test_pl!($ty, $field: $($name=$value,)+)
+    );
+
+    ($ty:ident, $field:ident: $($name:ident=$value:expr,)+) => ({
+        test_pl!($ty, $field ($($value,)+): $($name=$value,)+)
+    });
+
+    ($ty:ident, $field:ident ($arg:expr): $value:expr) => ({
+        let mut b = $ty::build();
+        mashup! { m["get" $field] = get_ $field; }
+        b.$field($arg);
+        let fapl = b.finish()?;
+        assert_eq!(fapl.$field(), $value);
+        m! { assert_eq!(fapl."get" $field()?, $value); }
+    });
+
+    ($ty:ident, $field:ident: $value:expr) => ({
+        test_pl!($ty, $field ($value): $value)
+    });
+}
+
+macro_rules! test_pl_common {
+    ($cls:ident, $plc:expr, $func:expr) => {
+        let pl_default = $cls::try_new()?;
+        assert_eq!(pl_default.class()?, $plc);
+        assert_eq!(pl_default, pl_default);
+
+        let mut b = $cls::build();
+        let pl = $func(&mut b)?;
+        assert_eq!(pl.class()?, $plc);
+        assert_eq!(pl, pl);
+        assert_ne!(pl, pl_default);
+
+        let pl2 = pl.copy();
+        assert_eq!(pl.class()?, $plc);
+        assert_eq!(pl2, pl);
+        assert_ne!(pl2, pl_default);
+    };
+}
+
+macro_rules! check_matches {
+    ($e:expr, $o:expr, $($p:tt)+) => (
+        match $e {
+            $($p)+ => $o,
+            ref e => panic!("assertion failed: `{:?}` does not match `{}`", e, stringify!($($p)+)),
+        }
+    )
 }
 
 #[test]
