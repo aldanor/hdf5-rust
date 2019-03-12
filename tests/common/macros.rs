c9bddb8fe6f9978e3ac193ec5e15b7dd99f3b16e
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-03-12 00:03:28 +0000

    Move datatype tests out of the main crate

---
+++ tests/common/macros.rs
@@ -0,0 +1,21 @@
+#[allow(unused_macros)]
+macro_rules! assert_err {
+    ($expr:expr, $err:expr) => {
+        match unsafe { $expr } {
+            Ok(_) => {
+                panic!("assertion failed: not an error in `{}`", stringify!($expr));
+            }
+            Err(ref value) => {
+                let desc = value.description().to_string();
+                if !desc.contains($err) {
+                    panic!(
+                        "assertion failed: error message `{}` doesn't contain `{}` in `{}`",
+                        desc,
+                        $err,
+                        stringify!($expr)
+                    );
+                }
+            }
+        }
+    };
+}
