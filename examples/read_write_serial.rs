7c500b4b99efaae98a2bb5252f12f86083a0b3f4
Philipp Reiter <preiter@bomaxx16.lfpn.ds.mpg.de>
2022-02-05 14:20:39 +0100

    update examples

--- examples/read_write_serial.rs
+++ examples/read_write_serial.rs
@@ -3,9 +3,28 @@
 //! ``` ignore
 //! cargo run --example read_write_serial
 //! ```
-use hdf5::{File, Result, Selection};
-use ndarray::{array, s, Array2, ArrayD, Ix2};
+use hdf5::{Dataset, Error, File, Result, Selection};
+use ndarray::{array, s, Array, Array2, ArrayView, Dimension};
 use std::convert::TryInto;
+
+fn write<'a, A, S, D>(dset: &Dataset, data: A, selection: S) -> Result<()>
+where
+    A: Into<ArrayView<'a, f64, D>>,
+    D: Dimension,
+    S: TryInto<Selection>,
+    S::Error: Into<Error>,
+{
+    dset.write_slice(data.into(), selection.try_into().map_err(|err| err.into())?)
+}
+
+fn read<S, D>(dset: &Dataset, selection: S) -> Result<Array<f64, D>>
+where
+    D: ndarray::Dimension,
+    S: TryInto<Selection>,
+    S::Error: Into<Error>,
+{
+    dset.read_slice(selection.try_into().map_err(|err| err.into())?)
+}
 
 fn main() -> Result<()> {
     // Output parameters
@@ -16,50 +35,34 @@ fn main() -> Result<()> {
     let shape = [3, 4];
     let data = Array2::<f64>::from_elem(shape, 2.);
     let slice = s![1..3, 1..3];
-    let data_slice = Array2::from_elem(data.slice(&slice).raw_dim(), 4.);
+    let mut data_slice = data.slice(&slice).to_owned();
+    data_slice.fill(4.0);
 
     // Write dataset
     {
-        // Open file (create or append)
-        let file = File::append(file_name)?;
+        let file = File::create(file_name)?;
+        let dset = file
+            .new_dataset::<f64>()
+            .shape(data.shape())
+            .create(dset_name)
+            .expect("Failed to create dataset.");
 
-        // Create or overwrite dataset
-        // It is better to use `unwrap_or_else` which is lazily evaluated
-        // than `unwrap_or`.
-        let dset = file.dataset(dset_name).unwrap_or_else(|_| {
-            file.new_dataset::<f64>()
-                .no_chunk()
-                .shape(data.shape())
-                .create(dset_name)
-                .expect("Failed to create dataset.")
-        });
+        // Write all the data
+        write(&dset, &data, ..)?;
 
-        // Write complete data
-        dset.write(&data)?;
-
-        // Write slice
-        let selection: Selection = slice.try_into()?;
-        dset.write_slice(&data_slice, selection)?;
-
-        // Close
-        file.close()?;
+        // Write a slice of the data
+        write(&dset, &data_slice, slice)?;
     }
 
-    //Read dataset
-    let data_read: Array2<f64> = {
-        // Open for read
+    // Read dataset
+    let data_read: Array2<f64>;
+    {
         let file = File::open(file_name)?;
-
-        // Read dynamically sized array
         let dset = file.dataset(dset_name)?;
-        let arr_dyn: ArrayD<f64> = dset.read_dyn::<f64>()?;
 
-        // Close
-        file.close()?;
-
-        // Dyn to static
-        arr_dyn.into_dimensionality::<Ix2>()?
-    };
+        // Read entire dataset
+        data_read = read(&dset, ..)?;
+    }
 
     assert_eq!(data_read, array![[2., 2., 2., 2.], [2., 4., 4., 2.], [2., 4., 4., 2.]]);
 
