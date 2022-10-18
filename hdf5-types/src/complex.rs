ed51217266f258c300ead6bea5667b5dc0506c75
Magnus Ulimoen <magnus@ulimoen.dev>
2022-10-18 19:03:51 +0200

    Test aligment of Complex

--- hdf5-types/src/complex.rs
+++ hdf5-types/src/complex.rs
@@ -21,10 +21,30 @@ unsafe impl<T: H5Type> H5Type for Complex<T> {
 mod tests {
     use super::*;
     use num_complex::{Complex32, Complex64};
+    use std::mem::size_of_val;
 
     #[test]
     fn complex() {
         assert_eq!(Complex32::type_descriptor().size(), 8);
+        assert_eq!(Complex32::type_descriptor().size(), size_of::<Complex32>());
         assert_eq!(Complex64::type_descriptor().size(), 16);
+        assert_eq!(Complex64::type_descriptor().size(), size_of::<Complex64>());
+    }
+
+    #[test]
+    fn alignment() {
+        use std::ptr::addr_of;
+
+        let a = Complex { re: 0.0, im: 1.0 };
+
+        assert_eq!(size_of_val(&a), size_of_val(&a.re) + size_of_val(&a.im));
+        assert_eq!(size_of_val(&a.re), size_of_val(&a.im));
+
+        let base: *const u8 = addr_of!(a).cast::<u8>();
+        let ptr_r = addr_of!(a.re).cast::<u8>();
+        let ptr_i = addr_of!(a.im).cast::<u8>();
+
+        assert_eq!(unsafe { ptr_r.offset_from(base) }, 0);
+        assert_eq!(unsafe { ptr_i.offset_from(base) }, size_of_val(&a.re) as isize);
     }
 }
