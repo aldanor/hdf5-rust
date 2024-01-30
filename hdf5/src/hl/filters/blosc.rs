a1624f7d53f1d998a446e8346bd675156674f6cc
Ivan Smirnov <github@ivan.smirnov.ie>
2024-01-30 20:19:37 +0000

    Fix compile warnings

--- hdf5/src/hl/filters/blosc.rs
+++ hdf5/src/hl/filters/blosc.rs
@@ -7,6 +7,7 @@ use hdf5_sys::h5p::{H5Pget_chunk, H5Pget_filter_by_id2, H5Pmodify_filter};
 use hdf5_sys::h5t::{H5Tclose, H5Tget_class, H5Tget_size, H5Tget_super, H5T_ARRAY};
 use hdf5_sys::h5z::{H5Z_class2_t, H5Z_filter_t, H5Zregister, H5Z_CLASS_T_VERS, H5Z_FLAG_REVERSE};
 
+use crate::error::H5ErrorCode;
 use crate::globals::{H5E_CALLBACK, H5E_PLIST};
 use crate::internal_prelude::*;
 
