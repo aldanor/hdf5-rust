b06368a15895c5774c330cd01d32b6b2d5074a00
James McNally <james@wiresmithtech.com>
2024-04-07 17:46:33 +0100

    Add version flags to HDF5-Types References

--- hdf5-types/src/h5type.rs
+++ hdf5-types/src/h5type.rs
@@ -384,6 +384,7 @@ unsafe impl H5Type for VarLenUnicode {
 pub enum Reference {
     Object,
     Region,
+    #[cfg(feature = "1.12.0")]
     Std,
 }
 
@@ -392,6 +393,7 @@ impl Reference {
         match self {
             Self::Object => mem::size_of::<hdf5_sys::h5r::hobj_ref_t>(),
             Self::Region => mem::size_of::<hdf5_sys::h5r::hdset_reg_ref_t>(),
+            #[cfg(feature = "1.12.0")]
             Self::Std => mem::size_of::<hdf5_sys::h5r::H5R_ref_t>(),
         }
     }
