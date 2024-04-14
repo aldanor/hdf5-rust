36fb0265084cbc0f24c5d0a1e252171c5b5787d3
James McNally <james@wiresmithtech.com>
2024-04-14 08:05:23 +0100

    fix: Remove StdReference try_clone
    The wrapper to H5RCopy is causing issues in some enviroments.
    
    This commit removes it for now as it isn't critical to the basic object
    implementation.

--- hdf5/src/hl/references/standard.rs
+++ hdf5/src/hl/references/standard.rs
@@ -2,9 +2,7 @@ use std::ops::Deref;
 
 use hdf5_sys::h5o::H5O_type_t;
 use hdf5_sys::h5r::H5R_type_t::H5R_OBJECT2;
-use hdf5_sys::h5r::{
-    H5R_ref_t, H5Rcopy, H5Rcreate_object, H5Rdestroy, H5Rget_obj_type3, H5Ropen_object,
-};
+use hdf5_sys::h5r::{H5R_ref_t, H5Rcreate_object, H5Rdestroy, H5Rget_obj_type3, H5Ropen_object};
 
 use super::ObjectReference;
 use crate::internal_prelude::*;
@@ -20,25 +18,11 @@ impl StdReference {
     fn ptr(&self) -> *const H5R_ref_t {
         std::ptr::addr_of!(self.inner)
     }
-
-    pub fn try_clone(&self) -> Result<Self> {
-        unsafe {
-            let mut dst = std::mem::MaybeUninit::uninit();
-            h5call!(H5Rcopy(self.ptr(), dst.as_mut_ptr()))?;
-            Ok(Self { inner: dst.assume_init() })
-        }
-    }
 }
 
 #[repr(transparent)]
 #[derive(Debug)]
 pub struct ObjectReference2(StdReference);
-
-impl ObjectReference2 {
-    pub fn try_clone(&self) -> Result<Self> {
-        self.0.try_clone().map(|std_ref| Self(std_ref))
-    }
-}
 
 impl Deref for ObjectReference2 {
     type Target = StdReference;
@@ -123,23 +107,6 @@ mod tests {
             let dsref = file.reference::<ObjectReference2>("ds").unwrap();
             let ds = file.dereference(&dsref).unwrap();
             assert!(matches!(ds, ReferencedObject::Dataset(_)));
-        })
-    }
-
-    #[test]
-    fn test_standard_reference_clone() {
-        with_tmp_file(|file| {
-            let orig_group = file.create_group("g").unwrap();
-            let ref1 = file.reference::<ObjectReference2>("g").unwrap();
-            let ref2 = ref1.try_clone().unwrap();
-
-            let ref2_group = ref2.dereference(&file).unwrap();
-            match ref2_group {
-                crate::ReferencedObject::Group(g) => {
-                    assert_eq!(g.name(), orig_group.name())
-                }
-                _ => panic!("Wrong reference type."),
-            }
         })
     }
 }
